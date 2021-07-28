5e7bdcca9befb5466d2a081b7f16301429675f46
Eric Charles <github@ericcharleslzq.com>
2021-07-28 12:08:02 +0800

    fix typo

--- workspace/proxmox/test.tf
+++ workspace/proxmox/test.tf
@@ -21,15 +21,15 @@ resource "local_file" "cloud_init_user_data_file" {
 }
 
 resource "null_resource" "cloud_init_config_files" {
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = data.vault_generic_secret.proxmox_credentials.data.password
+    host     = "10.10.30.168"
+  }
   provisioner "file" {
-    connection {
-      type     = "ssh"
-      user     = data.vault_generic_secret.proxmox_credentials.data.username
-      password = data.vault_generic_secret.proxmox_credentials.data.password
-      host     = "10.10.30.168"
-    }
     source      = local_file.cloud_init_user_data_file.filename
-    destination = "/mnv/pve/images/snippets/cloud-init-1.yml"
+    destination = "/mnt/pve/images/snippets/cloud-init-1.yml"
   }
 }
 
