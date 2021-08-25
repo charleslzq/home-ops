56bc782dfd46bea070811b23f42cdf244a430e67
Eric Charles <github@ericcharleslzq.com>
2021-08-25 10:21:31 +0800

    use vault to generate nomad token

--- workspace/console/job/terraform.tf
+++ workspace/console/job/terraform.tf
@@ -7,10 +7,6 @@ terraform {
   }
 }
 
-provider "nomad" {
-  address = "http://10.10.30.210:4646"
-}
-
 provider "consul" {}
 
 data "consul_keys" "config" {
@@ -24,4 +20,22 @@ provider "vault" {
   address         = "http://10.10.30.121:8200"
   skip_tls_verify = true
   token           = data.consul_keys.config.var.vault_token
+}
+
+// manually bootstrap acl and configure vault nomad engine
+resource "vault_nomad_secret_role" "terraform" {
+  backend   = "nomad"
+  role      = "terraform"
+  type      = "management"
+}
+
+data "vault_nomad_access_token" "token" {
+  backend = "nomad"
+  role    = vault_nomad_secret_role.terraform.role
+  depends_on = [vault_nomad_secret_role.terraform]
+}
+
+provider "nomad" {
+  address = "http://10.10.30.210:4646"
+  secret_id  = data.vault_nomad_access_token.token.secret_id
 }
