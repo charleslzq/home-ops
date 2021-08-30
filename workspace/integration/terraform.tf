59e9c9e385abc7e5449d26e728540159ed4b3186
Eric Charles <github@ericcharleslzq.com>
2021-08-30 16:16:50 +0800

    add comment

--- workspace/integration/terraform.tf
+++ workspace/integration/terraform.tf
@@ -1,3 +1,39 @@
+// bootstrap consul acl and setup token for consul server nodes and vault nodes. Unseal vault.
+// consul server policy:
+//   node_prefix "rayleigh" {
+//     policy = "write"
+//   }
+//   node_prefix "" {
+//     policy = "read"
+//   }
+//   service_prefix "" {
+//     policy = "read"
+//   }
+//
+// vault server policy:
+//   node_prefix "" {
+//     policy = "read"
+//   }
+//   service_prefix "" {
+//     policy = "read"
+//   }
+//   node_prefix "yuki" {
+//     policy = "write"
+//   }
+//   agent_prefix "yuki" {
+//     policy = "write"
+//   }
+//
+// vault service policy:
+//   service "yuki" {
+//      policy = "write"
+//   }
+//   key_prefix "vault/" {
+//     policy = "write"
+//   }
+//   session_prefix "yuki" {
+//     policy = "write"
+//   }
 terraform {
   backend "consul" {
     address = "127.0.0.1:8500"
