f54ba92afda734b8256b2a4b1434627f1885bcb4
Eric Charles <github@ericcharleslzq.com>
2021-08-30 14:27:28 +0800

    add consul gossip

---
+++ workspace/integration/modules/consul_vault/gossip.tf
@@ -0,0 +1,36 @@
+
+
+data "vault_generic_secret" "consul_setting" {
+  path = "home/consul/"
+}
+
+locals {
+  all = concat(var.consul_clients, var.consul_servers)
+  gossip_hcl = templatefile("${path.module}/files/gossip.hcl", {
+    encrypt = data.vault_generic_secret.consul_setting.data.gossip
+  })
+}
+
+resource "null_resource" "consul_gossip" {
+  count = length(local.all)
+  triggers = {
+    gossip_config = local.gossip_hcl
+  }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = local.all[count.index]
+    private_key = file("~/.ssh/id_rsa")
+    certificate = file("~/.ssh/id_rsa-cert.pub")
+  }
+  provisioner "file" {
+    content     = local.gossip_hcl
+    destination = "~/10.gossip.hcl"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv ~/10.gossip.hcl /etc/consul.d/",
+      "sudo systemctl restart consul",
+    ]
+  }
+}
