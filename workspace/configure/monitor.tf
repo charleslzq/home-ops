bb47839cb0458b9f8a4ad543d7ba13c3bbc3419d
Eric Charles <github@ericcharleslzq.com>
2021-09-20 10:20:05 +0800

    make prometheus scrape consul metrics

--- workspace/configure/monitor.tf
+++ workspace/configure/monitor.tf
@@ -25,10 +25,17 @@ data "consul_acl_token_secret_id" "prometheus" {
   accessor_id = consul_acl_token.prometheus.id
 }
 
+data "consul_nodes" "consul_servers" {
+  query_options {
+    datacenter = "rayleigh"
+  }
+}
+
 resource "null_resource" "prometheus" {
   provisioner "local-exec" {
     environment = {
       PROMETHEUS_CONSUL_TOKEN = data.consul_acl_token_secret_id.prometheus.secret_id
+      PROMETHEUS_CONSUL_IPS   = jsonencode([for node in data.consul_nodes.consul_servers.nodes : node.address if node.name != replace(node.name, "rayleigh", "")])
     }
     command = "ansible-playbook _monitor.yml"
   }
